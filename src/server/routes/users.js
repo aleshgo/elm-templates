2cf47231ad9f1ee8f18f998827b396ca65873704
Alexander Newman <aleshgo@gmail.com>
2016-10-15 09:24:27 +0300

    Work on creation of registraion API

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,26 +1,33 @@
 const jsonfile = require('jsonfile')
 const register_schema = require('../validation/register')
 const validate = require('../validate');
+const _ = require('lodash');
 
 
 module.exports = function(router) {
   router.post('/users', validate(register_schema), function(req, res) {
-    var file = 'data.json'
+    var file = process.env.DATA_FILE;
 
     // read data
     jsonfile.readFile(file, function(err, users) {
       if(err) {
-        return res.serverError(err);
+        users = []
       }
+
+      const profile = _.pick(req.body, 'username', 'password');
+
       // check if user exists
+      if (_.find(users, { username: profile.username })) {
+        return res.badRequest({ username: "\"username\" already exists"});
+      }
 
       // add user
-      users.push(req.body);
+      users.push(profile);
       jsonfile.writeFile(file, users, function (err) {
         if(err) {
           return res.serverError(err);
         }
-        return res.created({ JWT: 'ssss' });
+        return res.created({ TOKEN: 'token' });
       });
     });
   });
