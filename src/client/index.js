0d9598c5f5c846b9fc87eac38ed06932069c6108
Alexander Newman <aleshgo@gmail.com>
2016-10-18 04:11:52 +0300

    Add css

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+require('ace-css/css/ace.css');
+require('font-awesome/css/font-awesome.css');
+require('./style.css');
+
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
